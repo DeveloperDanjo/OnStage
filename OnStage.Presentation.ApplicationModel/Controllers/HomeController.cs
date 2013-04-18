d308c4f6aca3a8ac55d1b73f567c9ae07c8736a2
Daniel Joseph <getoveritdan@gmail.com>
2013-04-18 19:35:09 -0400

    Initial Commit

---
+++ OnStage.Presentation.ApplicationModel/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace OnStage.Presentation.ApplicationModel.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ OnStage.Presentation.CompositionRoot/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OnStage.Presentation.CompositionRoot.MvcApplication" Language="C#" %>
