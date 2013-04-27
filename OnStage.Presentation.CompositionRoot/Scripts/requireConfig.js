df7a25192d2c49fa03943da06ccec6f778fb7f02
Daniel Joseph <getoveritdan@gmail.com>
2013-04-27 16:11:06 -0400

    Add lodash, remove jquery ui

--- OnStage.Presentation.CompositionRoot/Scripts/requireConfig.js
+++ OnStage.Presentation.CompositionRoot/Scripts/requireConfig.js
@@ -2,14 +2,16 @@
     baseUrl: "/Scripts",
     paths: {
         jquery: 'jquery-1.9.1',
-        'jquery-ui': 'jquery-ui-1.10.2',
         'jquery-unobtrusive': 'jquery.unobstrusive-ajax',
         'jquery-validate': 'jquery.validate',
         'jquery-validate-unobstrusive': 'jquery.validate.unobstrusive',
         signalr: 'jquery.signalR-1.0.1',
         hubs: '/signalr/hubs?',
         bootstrap: 'bootstrap',
-        modernizr: 'modernizr-2.6.2'
+        modernizr: 'modernizr-2.6.2',
+        underscore: 'lodash',
+
+        text: 'text'
     },
     map: {
         '*': {
@@ -19,9 +21,6 @@
     shim: {
         jquery: {
             init: function () { return this.$.noConflict(); }
-        },
-        'jquery-ui': {
-            deps: ['jquery'],
         },
         'jquery-unobstrusive': {
             deps: ['jquery']
